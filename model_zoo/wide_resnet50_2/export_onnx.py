6450a6d3fd32273e6fbdb31603ba8a52bbe0ef40
Shuji Urano <shuji.urano.fg@gmail.com>
2025-01-15 13:09:15 +0900

    Modify code

--- model_zoo/wide_resnet50_2/export_onnx.py
+++ model_zoo/wide_resnet50_2/export_onnx.py
@@ -12,6 +12,6 @@ from torchvision import models, transforms
 #from torchvision.models import wide_resnet50_2
 
 model = models.wide_resnet50_2(weights=models.Wide_ResNet50_2_Weigths.IMAGENET1K_V2)
-input = torch.randn(1,, 1,, 32, 32)
+input = torch.randn(1, 1, 32, 32)
 onnx_model = torch.onnx.dynamo_export(model, input)
 onnx_model.save("wide_resnet50_2-IMAGENET1K_V2.onnx")
